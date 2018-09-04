0d045640be3776a61a2374c67569d35757fed69f
kevinbai0 <kevin.c.bai0@gmail.com>
2018-09-04 17:49:22 -0400

    Added production condition

--- index.js
+++ index.js
@@ -13,8 +13,14 @@ var router = express.Router();
 
 router.use(express.json());
 
-app.get("/", (req, res) => res.send("It works!"));
-
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 // get clients
 router.get('/clients', (req, res) => {
     Client.find({}, (err, clients) => {
