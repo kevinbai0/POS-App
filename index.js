87b327b9a1297af527958745773150fc013f6896
kevinbai0 <kevin.c.bai0@gmail.com>
2018-09-04 20:32:54 -0400

    removing client side

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ var router = express.Router();
 
 router.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
+/*if (process.env.NODE_ENV === "production") {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'front-end/build')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'front-end/build', 'index.html'));
     });
-}
+}*/
 
 
 
@@ -160,5 +160,5 @@ router.put("/products/:product_id", (req, res) => {
 })
 app.use("/api", router);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Running on port " + port));
